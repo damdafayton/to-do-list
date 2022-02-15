1da42dc9780f0dcca0dcc00f437e7764343b8a6c
backtofayton <yucelolcay@gmail.com>
2022-02-16 00:38:57 +0200

    Fix webpack

--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@
 import './style.scss';
 import populateList from './modules/populateList';
 import './modules/removeTask';
-import './modules/addTask';
+import addTaskHandler from './modules/addTask';
 import { addEditHandlers } from './modules/editTask';
 
 // populate UI on page load
 const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
 tasks.forEach((task) => populateList(task));
 addEditHandlers();
+
+// create task handler
+window.addEventListener('DOMContentLoaded', addTaskHandler);
 
 // prevent bad ui during load
 window.addEventListener('load', () => {
--- src/modules/addTask.js
+++ src/modules/addTask.js
@@ -1,28 +1,30 @@
 import populateList from './populateList';
 
-// new task - keyboard listener
-const inputTask = document.querySelector('#list-container > div input[type=text]');
-inputTask.addEventListener('keydown', (e) => {
-  if (e.code === 'Enter') {
+export default function addTaskHandler() {
+  // new task - keyboard listener
+  const inputTask = document.querySelector('#create-task-text');
+  inputTask.addEventListener('keydown', (e) => {
+    if (e.code === 'Enter') {
+      const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+      const newTask = { description: inputTask.value, completed: false, index: tasks.length };
+      tasks.push(newTask);
+      localStorage.setItem('tasks', JSON.stringify(tasks));
+      populateList(newTask);
+      inputTask.value = '';
+      console.log(tasks);
+    }
+  });
+
+  // new task click listener
+  const formAddBtn = document.querySelector('#list-container > div > span');
+  formAddBtn.addEventListener('click', (e) => {
+    const taskInput = e.target.previousElementSibling;
     const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
-    const newTask = { description: inputTask.value, completed: false, index: tasks.length };
+    const newTask = { description: taskInput.value, completed: false, index: tasks.length };
     tasks.push(newTask);
     localStorage.setItem('tasks', JSON.stringify(tasks));
     populateList(newTask);
-    inputTask.value = '';
+    taskInput.value = '';
     console.log(tasks);
-  }
-});
-
-// new task click listener
-const formAddBtn = document.querySelector('#list-container > div > span');
-formAddBtn.addEventListener('click', (e) => {
-  const taskInput = e.target.previousElementSibling;
-  const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
-  const newTask = { description: taskInput.value, completed: false, index: tasks.length };
-  tasks.push(newTask);
-  localStorage.setItem('tasks', JSON.stringify(tasks));
-  populateList(newTask);
-  taskInput.value = '';
-  console.log(tasks);
-});
+  });
+}
